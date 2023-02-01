76c5b34bcd195a113556a9ad09d9b249ae82dd2c
RockThePandora <92234814+RockThePandora@users.noreply.github.com>
2023-02-01 13:13:29 -0800

    Delete buildQMOD.ps1

--- buildQMOD.ps1
+++
@@ -1,7 +0,0 @@
-# Builds a .zip file for loading with BMBF
-& ./build.ps1
-Compress-Archive -Path  "./extern/libs/libbeatsaber-hook_3_8_1.so",`
-                        "./build/libmrcplus.so",`
-                        "./cover.png",`
-                        "./mod.json" -DestinationPath "./MRCPlus.zip" -Update
-& copy-item -Force "./MRCPlus.zip" "./MRCPlus.qmod"

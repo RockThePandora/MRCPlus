d2de95d46f88e554badb57f08a3fc311f460b4cb
RockThePandora <92234814+RockThePandora@users.noreply.github.com>
2023-02-01 08:39:14 -0800

    Update MRCPlusFlowCoordinator.cpp

--- src/UI/SettingsMenu/MRCPlusFlowCoordinator.cpp
+++ src/UI/SettingsMenu/MRCPlusFlowCoordinator.cpp
@@ -42,6 +42,6 @@ void MRCPlusFlowCoordinator::UpdateSettingsVisibility(bool leftVisible, bool rig
 void MRCPlusFlowCoordinator::BackButtonWasPressed(HMUI::ViewController* topView)
 {
     getConfig().Write();
-    HMUI::FlowCoordinator* settingsFC = QuestUI::GetModSettingsFlowCoordinator();
+    //HMUI::FlowCoordinator* settingsFC = QuestUI::GetModSettingsFlowCoordinator();
     settingsFC->DismissFlowCoordinator(this, (int)0, nullptr, false);
 }
